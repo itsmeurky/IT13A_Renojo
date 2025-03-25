bd1c3f7a9d46c233e1356c8e9f66f21b84c6dc5d
michaella <michaelladrenojo@gmail.com>
2025-03-25 08:35:20 +0800

    Added Java Arrays

---
+++ Renojo_IT13A_Activities/src/Midterm/Array1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MIDTERM;
+
+/**
+ *
+ * @author user
+ */
+public class Array1 {
+     public static void main (String [] args ){
+    String letter []= {"A", "B", "C", "D", "E", "F", "G"} ;
+        for (int i = 0; i < letter.length; i++){       
+        System.out.print (letter[i] + ", ");
+        }
+     }
+}
